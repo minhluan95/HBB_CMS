6c0c41e46c6743ad675aad770fd923397badb76d
minhluan95 <cuminhluan1995@gmail.com>
2016-09-15 13:44:11 +0700

    Ver 1.1

---
+++ HBB_CMS/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HBB_CMS.Areas.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        //
+        // GET: /Admin/Post/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AllPost()
+        {
+            return View();
+        }
+        public ActionResult AddNew()
+        {
+            return View();
+        }
+        public ActionResult Categories()
+        {
+            return View();
+        }
+        public ActionResult Tags()
+        {
+            return View();
+        }
+        public ActionResult SidebarPartial() 
+        {
+            return PartialView();
+        }
+	}
+}
